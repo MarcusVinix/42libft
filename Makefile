9a25e45bf74fa1b5e5bb41203e2b407753fd3daa
MarcusVinix <vi.ni159marcus@gmail.com>
2021-05-15 17:11:09 -0300

    add ft_strlcat

--- Makefile
+++ Makefile
@@ -20,6 +20,7 @@ SRCS =	ft_bzero.c \
 		ft_memset.c \
 		ft_strlen.c \
 		ft_strlcpy.c \
+		ft_strlcat.c \
 		libft.h \
 		testes.c
 
--- libft.h
+++ libft.h
@@ -15,4 +15,5 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 #endif
